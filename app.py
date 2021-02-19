04f29a23143318027ff57d8b159fda6e8cf26e23
Peter Haas <peter@peterhaas.me>
2021-02-19 15:33:52 -0500

    moved application object into it's own file

---
+++ app.py
@@ -0,0 +1,7 @@
+from werkzeug.middleware.dispatcher import DispatcherMiddleware # use to combine each Flask app into a larger one that is dispatched based on prefix
+from flask_1 import app as flask_app_1
+from flask_2 import app as flask_app_2
+
+application = DispatcherMiddleware(flask_app_1, {
+    '/flask_app_2': flask_app_2
+})
--- run.py
+++ run.py
@@ -1,11 +1,5 @@
-from werkzeug.middleware.dispatcher import DispatcherMiddleware # use to combine each Flask app into a larger one that is dispatched based on prefix
 from werkzeug.serving import run_simple # werkzeug development server
-from flask_1 import app as flask_app_1
-from flask_2 import app as flask_app_2
-
-application = DispatcherMiddleware(flask_app_1, {
-    '/flask_app_2': flask_app_2
-})
+from app import application
 
 if __name__ == '__main__':
     run_simple('localhost', 5000, application, use_reloader=True, use_debugger=True, use_evalex=True)
